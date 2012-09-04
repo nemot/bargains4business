cbd5705e378cf5f618d1bf8749addb6f00514e9f
nemot <gnemot@gmail.com>
2012-09-04 13:17:31 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bargains4business::Application.config.secret_token = 'e096c8679b6c6b0af8d393d57fecafd977cf5c8fcae839b2783041aed758844e4462d6ede4d99fbba98367b6a67d32b26a878286cae55134384f5f4619fe19be'
